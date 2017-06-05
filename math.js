1fa6f77d325eb7c271c7a0e6e9f0539403496c0c
Anton <inprogger@gmail.com>
2017-06-05 18:09:40 +0700

    50

---
+++ math.js
@@ -0,0 +1,25 @@
+var TelegramBot = require('node-telegram-bot-api'),
+	token = '394771171:AAEF8E04lO7whVWH9fWJYjHhOm6hLCmnSyA';
+
+var bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', function(msg){
+	var id = msg.chat.id;
+	if (msg.text == '/help')
+	{
+		bot.sendMessage(id, 'To find the expression enter it');
+	}
+	else{
+		if (/^[-+()*\/0-9\s]+$/.test(msg.text)) {
+			try {
+				bot.sendMessage(id, eval(msg.text));
+			}
+			catch (e) {
+				bot.sendMessage(id, 'Syntax error, please, try again');
+			}
+		}
+		else {
+			bot.sendMessage(id, 'Incorret value, please, try again');
+		}
+	}
+});
